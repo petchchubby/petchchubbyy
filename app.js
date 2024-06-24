ddf8fe997b7d116312181b92a51b86cd3f1314c6
petchchubby <petchchubbyy@gmail.com>
2024-06-25 00:57:31 +0700

    finished navbar

--- app.js
+++ app.js
@@ -5,3 +5,17 @@ function showPage() {
     document.getElementById('preLoader').style.display = 'none';
     document.getElementById('pageContent').style.display = 'block';
 }
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    // ถ้า scroll ลงมากกว่า 20px ให้เพิ่ม class "scroll" ให้กับ Navbar
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").classList.add("scroll");
+    } else {
+        // ถ้า scroll ขึ้นมาสูงกว่า 20px ให้ลบ class "scroll" ทิ้ง
+        document.getElementById("navbar").classList.remove("scroll");
+    }
+}
