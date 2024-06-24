b16e83bbe9dbb1e8d6d610f30bc13c7d8f425d79
petchchubby <petchchubbyy@gmail.com>
2024-06-25 05:01:34 +0700

    final1

--- app.js
+++ app.js
@@ -19,3 +19,25 @@ function scrollFunction() {
         document.getElementById("navbar").classList.remove("scroll");
     }
 }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+document.addEventListener('DOMContentLoaded', function() {
+    const returnToTop = document.getElementById('returnToTop');
+
+    returnToTop.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+});
