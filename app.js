f6a8daa55379caa2388e87636b34decf3a8c8215
petchchubby <petchchubbyy@gmail.com>
2024-06-25 01:57:21 +0700

    finished hero section

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ function showPage() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-    // ถ้า scroll ลงมากกว่า 20px ให้เพิ่ม class "scroll" ให้กับ Navbar
+   
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("navbar").classList.add("scroll");
     } else {
-        // ถ้า scroll ขึ้นมาสูงกว่า 20px ให้ลบ class "scroll" ทิ้ง
+        
         document.getElementById("navbar").classList.remove("scroll");
     }
 }
